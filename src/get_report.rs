b6e7a998365f3c5c810fe189f0e961e4aa321bd0
Rafael Cardoso <racasantos@icloud.com>
2025-01-15 16:02:06 -0500

    Merge pull request #4
    * sldfkajsd alsdjfhl; asdfe

--- src/get_report.rs
+++ src/get_report.rs
@@ -1,8 +1,10 @@
 use std::io::Read;
+use base64::engine::general_purpose::STANDARD;
+use base64::Engine;
 use flate2::bufread::GzDecoder;
 use crate::get_xml_by_report::get_xml;
 
-pub fn get_report(url: &str,  username: &str, password: &str, report_path: &str, output_format: &str, params: Vec<(&str, &str)>)
+pub fn get_report(url: &str, username: &str, password: &str, report_path: &str, output_format: &str, params: Vec<(&str, &str)>)
                   -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::new();
     let body = get_xml(report_path, output_format, params);
@@ -11,7 +13,7 @@ pub fn get_report(url: &str,  username: &str, password: &str, report_path: &str,
         .post(url)
         .header("Content-Type", "text/xml")
         .header("SOAPAction", "runReport")
-        .header("Authorization", format!("Basic {}", base64::encode(format!("{}:{}", username, password))))
+        .header("Authorization", format!("Basic {}", STANDARD.encode(format!("{}:{}", username, password))))
         .body(body)
         .send()?;
 
@@ -25,12 +27,12 @@ pub fn get_report(url: &str,  username: &str, password: &str, report_path: &str,
     println!("{:?}", decompressed_body);
     let doc = roxmltree::Document::parse(&decompressed_body)?;
     let node = doc.root_element();
-    let ns = node.lookup_namespace_uri(None);
+    let _ns = node.lookup_namespace_uri(None);
     let mut result = Vec::new();
     for elem in node.descendants() {
         if elem.has_tag_name("reportBytes") {
             let text = elem.text().unwrap();
-            result = base64::decode(text)?;
+            result = STANDARD.decode(text)?;
         }
     }
     Ok(result)
